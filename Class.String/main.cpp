5815d3760ac06846ca892abf8b4dfc2c18499801
Igor Kind <86460417+ngessda@users.noreply.github.com>
2021-08-16 11:32:41 +0900

    none

--- Class.String/main.cpp
+++ Class.String/main.cpp
@@ -72,14 +72,57 @@ public:
 
 		return newStr;
 	}
+	bool operator ==(const MyString& other)
+	{
+		if (this->length != other.length)
+		{
+			return false;
+		}
+
+		for (int i = 0; i < this->length; i++)
+		{
+			if (this->str[i] != other.str[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool operator !=(const MyString& other)
+	{
+		return !(this->operator==(other));
+	}
+
 	void Print()
 	{
-		std::cout << str;
+		std::cout << str << std::endl;
 	}
+
 	~MyString()
 	{
 		delete[] str;
 	}
+
+	int Length()
+	{
+		return length;
+	}
+
+	char& operator[](int index)
+	{
+		return this->str[index];
+	}
+
+
+	MyString(MyString&& other)
+	{
+		this->length = other.length;
+		this->str = other.str;
+
+		other.str = nullptr;
+	}
+
 
 private:
 	char* str;
@@ -95,6 +138,15 @@ int main(int argc, char* argv[])
 	MyString a;
 	a = str + str1;
 	a.Print();
+	std::cout << str.Length() << std::endl;
+	std::cout << str1.Length() << std::endl;
+	std::cout << a.Length() << std::endl;
+	bool equal = str == str1;
+	std::cout << "\n" << equal << std::endl;
+	bool a_equal = str != str1;
+	std::cout << "\n" << a_equal << std::endl;
+	str[0] = 'Q';
+	str.Print();
 	return 0;
 	std::cin.get();
 }
